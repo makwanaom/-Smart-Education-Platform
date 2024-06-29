986e47d0964f67cb013fd5b15becd8afecf3d767
mainavinhoon <navinrawat11032004@gmail.com>
2024-06-29 19:55:58 +0530

    HOmePage section changed it bit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -133,20 +133,7 @@ function Home() {
 
         <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
           {/* Job that is in Demand - Section 1 */}
-          <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
-            <div className="text-4xl font-semibold lg:w-[45%] ">
-              Get the skills you need for a{" "}
-              <HighlightText text={"job that is in demand."} />
-            </div>
-            <div className="flex flex-col items-start gap-10 lg:w-[40%]">
-              <div className="text-[16px]">
-              Modern Smart Education sets its own standards. Today, being a competitive specialist demands more than just professional skills.
-              </div>
-              <CTAButton active={true} linkto={"/signup"}>
-                <div className="">Learn More</div>
-              </CTAButton>
-            </div>
-          </div>
+          
 
           {/* Timeline Section - Section 2 */}
           <TimelineSection />
