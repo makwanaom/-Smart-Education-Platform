5f135d09ce5dbc818808141b3ca0bd6f0cb7078d
makwanaom <117265350+makwanaom@users.noreply.github.com>
2024-06-29 19:17:22 +0530

    remove some glich

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -134,13 +134,18 @@ const About = () => {
         <ContactFormSection />
       </section>
 
-      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+      {/* <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white"> */}
         {/* Reviews from Other Learners */}
-        <h1 className="text-center text-4xl font-semibold mt-8">
+        {/* <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
-        </h1>
-        <ReviewSlider />
-      </div>
+        </h1> */}
+        {/* <ReviewSlider /> */}
+      {/* </div> */}
+
+
+  <br />
+  <br />
+
       <Footer />
     </div>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,13 +19,8 @@ const Contact = () => {
           <ContactForm />
         </div>
       </div>
-      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Reviws from Other Learner */}
-        <h1 className="text-center text-4xl font-semibold mt-8">
-          Reviews from other learners
-        </h1>
-        <ReviewSlider />
-      </div>
+     <br />
+     <br />
       <Footer />
     </div>
   )
